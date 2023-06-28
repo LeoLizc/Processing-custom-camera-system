3f3dde4bae341a1b663cd4388a427b5b2674e73d
LeoLizc <ldlizcano@uninorte.edu.co>
2023-06-27 22:49:16 -0500

    3d environment

--- src/Main.java
+++ src/Main.java
@@ -1,13 +1,18 @@
-
 import processing.core.PApplet;
+import processing.core.PConstants.*;
+
 public class Main extends PApplet{
 
     public void settings(){
-        size(600, 600);
+        size(400, 400, P3D);
     }
 
     public void setup(){
-        rect(0, 0, 100, 100);
+//        size(400,400, PConstants.P3D);
+        translate(232, 192, 0);
+        rotateY(0.5f);
+        noFill();
+        box(160);
     }
 
     public void draw(){
