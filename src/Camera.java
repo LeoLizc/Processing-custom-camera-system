43fe9c211e43134126fb99f65bbb0031596d3beb
LeoLizc <ldlizcano@uninorte.edu.co>
2023-06-27 23:48:45 -0500

    2d movement

---
+++ src/Camera.java
@@ -0,0 +1,66 @@
+import processing.core.PApplet;
+import processing.core.PVector;
+
+public class Camera {
+    static final int W=0,A=1,S=2,D=3;
+    final boolean[] keysPressed = new boolean[4];
+
+    PApplet p;
+    PVector position;
+    float velocity = 3;
+
+    public Camera(PApplet parent) {
+        p = parent;
+        position = new PVector();
+    }
+
+    public void startMove(char key){
+        updateMove(key, true);
+    }
+
+    public void stopMove(char key){
+        updateMove(key, false);
+    }
+
+    public void updateMove(char key, boolean value) {
+
+        switch (key) {
+            case 'w':
+                position.y += 5;
+                keysPressed[W] = value;
+                break;
+            case 'a':
+                position.x -= 5;
+                keysPressed[A] = value;
+                break;
+            case 's':
+                position.y -= 5;
+                keysPressed[S] = value;
+                break;
+            case 'd':
+                position.x += 5;
+                keysPressed[D] = value;
+        }
+
+    }
+
+    public void updateCamera(){
+//        Update position
+        if(keysPressed[W]){
+            position.y += velocity;
+        }
+        if(keysPressed[A]){
+            position.x -= velocity;
+        }
+        if(keysPressed[S]){
+            position.y -= velocity;
+        }
+        if(keysPressed[D]){
+            position.x += velocity;
+        }
+    }
+
+    public void processCamera(){
+        p.translate(-position.x, position.y, position.z);
+    }
+}
--- src/Main.java
+++ src/Main.java
@@ -1,23 +1,46 @@
 import processing.core.PApplet;
-import processing.core.PConstants.*;
 
 public class Main extends PApplet{
-
+    Camera camera;
     public void settings(){
         size(400, 400, P3D);
+        camera = new Camera(this);
     }
 
     public void setup(){
 //        size(400,400, PConstants.P3D);
+    }
+
+    public void draw(){
+        update();
+//        rotateX(PI/0.4f);
+        camera.processCamera();
+        render();
+    }
+
+    void update(){
+        camera.updateCamera();
+    }
+
+    void render(){
+        background(240,240,240);
+        drawCube();
+    }
+
+    void drawCube(){
         translate(232, 192, 0);
         rotateY(0.5f);
         noFill();
         box(160);
     }
-
-    public void draw(){
-
+    public void keyPressed(){
+        camera.startMove(key);
     }
+
+    public void keyReleased(){
+        camera.stopMove(key);
+    }
+
     public static void main(String[] args){
         PApplet.main("Main");
     }
